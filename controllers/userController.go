4d2e2b0197d1f0917c6ea9409efae22c5b8e7cf5
CABON-TECH <kishushubonifacembogho@gmail.com>
2024-02-27 02:59:21 +0300

    ...

--- controllers/userController.go
+++ controllers/userController.go
@@ -106,7 +106,10 @@ func Login(c *gin.Context) {
 }
 
 func Validate(c *gin.Context) {
+
+	user, _ := c.Get("user")
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "I'm logged in",
+		"message": user,
 	})
 }
